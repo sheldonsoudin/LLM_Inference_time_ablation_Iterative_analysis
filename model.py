6e1abd02d4b05d8dd0150c2b78d3de122c5c63eb
Sheldon Soudin <sheldon.soudin@gmail.com>
2026-03-19 11:32:02 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -1,57 +1,55 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyNghVTQ9m6CmaUbMtqy0if0",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/sheldonsoudin/LLM_Inference-time_ablation__Iterative_analysis/blob/main/model.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "markdown",
-      "source": [
-        "This notebook contains the definition."
-      ],
-      "metadata": {
-        "id": "uqnvjclhaVBf"
-      }
-    },
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "DeWL-5rWaNJ0"
-      },
-      "outputs": [],
-      "source": []
-    },
-    {
-      "cell_type": "code",
-      "source": [],
-      "metadata": {
-        "id": "gcBcx2QQaTBX"
-      },
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+
+
+"""
+Sources: 
+- nanoGPT  (Karpathy):   fused QKV, weight tying, Flash Attention, bias flag
+- Raschka LLMs-from-Scratch: explicit custom LayerNorm, transparent GELU
+- GPT-3 (Brown et al.):  alternating dense/sparse attention, 2048 ctx, scalekk
+
+"""
+
+import math 
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+from dataclasses import dataclass
+from typing import Optional 
+
+@dataclass
+class GPTConfig: 
+    #Modlel dimensions 
+    n_layers: int = 128 #nbr of trasformer blocks  
+    n_heads = 128  #nbr of attention heads
+    n_embd = 16384  
+    d_ffn = 65536 # x4 rule 
+    context_len = 2048 #gpt-3's context window
+    vocab_size: 
+
+
+
+
+class GPT(nn.module): 
+    def forward(): 
+
+
+
+class MLP(nn.module): 
+
+
+class LayerNorm(): 
+
+
+
+class Block(nn.module): 
+
+    def __init__(self,config:GPTConfig,layer_idx:int): 
+        super().__init__()
+        self.ln_1 = LayerNorm(config.n_embd,bias=config.bias)
+        self.attn = CausalSelfAttention(config,layer_idx) 
+        self.ln_2 = LayerNorm(config.embd,bias=config.bias)
+        self.mlp = MLP(config) 
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor: 
+        x = x+ self.attn(self.len_1(x)) # attention residual 
+        x = x+ self.mlp(slef.ln_2(x)) # MLP residual 
+
