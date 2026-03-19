03f53a5ee4801c1d377754e5aa7bfd8d693d38d1
Sheldon Soudin <sheldon.soudin@gmail.com>
2026-03-19 10:06:02 -0400

    Created using Colab

---
+++ model.py
@@ -0,0 +1,57 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNghVTQ9m6CmaUbMtqy0if0",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/sheldonsoudin/LLM_Inference-time_ablation__Iterative_analysis/blob/main/model.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "This notebook contains the definition."
+      ],
+      "metadata": {
+        "id": "uqnvjclhaVBf"
+      }
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "DeWL-5rWaNJ0"
+      },
+      "outputs": [],
+      "source": []
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "gcBcx2QQaTBX"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
